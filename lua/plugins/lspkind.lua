8167d1d46b75e5cba0a184459b2d23fb80073174
tungduong150105 <tungduong150105@gmail.com>
2026-01-30 10:00:57 +0700

    feat: add lspkind plugin

---
+++ lua/plugins/lspkind.lua
@@ -0,0 +1,36 @@
+return {
+  "onsails/lspkind.nvim",
+  config = function()
+    require('lspkind').init({
+      mode = 'symbol_text',
+      preset = 'default',
+      symbol_map = {
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰜢",
+        Variable = "󰀫",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "󰑭",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "󰈇",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "",
+      },
+    })
+  end
+}
