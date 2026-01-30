c17bbfd28de3ea22afdce294d35c1447d10931eb
tungduong150105 <tungduong150105@gmail.com>
2026-01-30 09:11:38 +0700

    feat: switch from nvim-tree to neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons"
+  },
+  lazy = false,
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>")
+  end
+}
