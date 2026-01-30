33755fc5026203638e1d6e0e98a1eb7ec5a19cb8
tungduong150105 <tungduong150105@gmail.com>
2026-01-30 10:01:39 +0700

    feat: integrate lspkind with blink-cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,13 +1,31 @@
 return {
   'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
   version = '1.*',
   opts = {
     keymap = { preset = 'default' },
     appearance = {
       nerd_font_variant = 'mono'
     },
-    completion = { documentation = { auto_show = true } },
+    completion = {
+      documentation = { auto_show = true },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                return require('lspkind').symbol_map[ctx.kind] or ''
+              end,
+            },
+          },
+          columns = {
+            { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 }
+          }
+        },
+      }
+    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
